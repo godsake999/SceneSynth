820946a663df4ec311a77f2f0a5879ad6a4b4fe3
godsake999 <adinadin10@gmail.com>
2026-01-27 15:00:23 +0630

    Switch to Stable Diffusion XL - Playground v2.5 no longer available

--- server/tts_server.py
+++ server/tts_server.py
@@ -20,7 +20,7 @@ FREESOUND_BASE_URL = 'https://freesound.org/apiv2'
 
 # Hugging Face API Configuration
 HF_API_TOKEN = os.environ.get('HF_API_TOKEN', '')
-HF_IMAGE_MODEL = 'playgroundai/playground-v2.5-1024px-aesthetic'
+HF_IMAGE_MODEL = 'stabilityai/stable-diffusion-xl-base-1.0'
 HF_API_URL = f'https://api-inference.huggingface.co/models/{HF_IMAGE_MODEL}'
 
 CORS(app)
